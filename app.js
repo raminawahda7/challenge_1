e51b304e9d3741bef7040ce3527456ec678e0893
RamiNawahda <rami.nawahda@gmail.com>
2020-11-02 17:59:04 +0200

    Same as last commit with changes

--- app.js
+++ app.js
@@ -2,9 +2,26 @@
 // 1- If the player-1  choose  X and click on div the role will change to player-2 and so on.
 // 2- When Any diagnol happen that mean one of them win. 
 
-var ticMatrix = [
-    0,0,0,
-    0,0,0,
-    0,0,0
-];
+// var ticMatrix = [
+//     0, 0, 0,
+//     0, 0, 0,
+//     0, 0, 0
+// ];
 
+let statusGame=true;
+let firstMove = 'X';
+let message ='';
+// var squares = document.querySelector('.parent')
+// console.log(squares.children)
+////////create div///////////
+//  var winnerDiv = document.createElement('div');
+//  winnerDiv.className='winner';
+//  winnerDiv.id='winner1';
+//  var newDivText = document.createTextNode('Winner is :');
+//  winnerDiv.appendChild(newDivText);
+
+//document.getElementById("myDIV").style.display = "none";
+
+if(document.getElementById('div1').innerText==='X'&& document.getElementById('div5').innerText==='X' &&document.getElementById('div9').innerText==='X'){
+
+}
