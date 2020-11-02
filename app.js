9cb7ae67793e484205d9d3577552331a66f10956
RamiNawahda <rami.nawahda@gmail.com>
2020-11-02 15:56:02 +0200

    Test commit. Please ignore

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
+// We have two player one of them will choose X, the second will choose O.
+// 1- If the player-1  choose  X and click on div the role will change to player-2 and so on.
+// 2- When Any diagnol happen that mean one of them win. 
 
+var ticMatrix = [
+    0,0,0,
+    0,0,0,
+    0,0,0
+];
 
